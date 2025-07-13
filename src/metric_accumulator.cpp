56392181cb736991deb82def06134783b2509e83
Mikhail Zykov <zmsmihail@yandex.ru>
2025-07-13 11:57:47 +0300

    Fix names

--- src/metric_accumulator.cpp
+++ src/metric_accumulator.cpp
@@ -4,9 +4,10 @@ namespace analyser::metric_accumulator {
 
 void MetricAccumulator::AccumulateNextResults(const metric::MetricResults &metric_results) const {
     std::ranges::for_each(accumulators_, [&metric_results](auto &p) {
-        std::ranges::for_each(metric_results, [&p](const auto &metric_result) {
-            if (std::get<0>(p) == metric_result.metric_name) {
-                std::get<1>(p)->Accumulate(metric_result);
+        const auto &[metric_name, acc] = p;
+        std::ranges::for_each(metric_results, [&metric_name, &acc](const auto &metric_result) {
+            if (metric_name == metric_result.metric_name) {
+                acc->Accumulate(metric_result);
             }
         });
     });
