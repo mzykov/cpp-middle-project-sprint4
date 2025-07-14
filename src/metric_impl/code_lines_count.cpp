41799acf74466aeb514d1741e3b59deb9e8ae441
Mikhail Zykov <zmsmihail@yandex.ru>
2025-07-14 12:38:36 +0300

    Fix code lines count metric

--- src/metric_impl/code_lines_count.cpp
+++ src/metric_impl/code_lines_count.cpp
@@ -1,3 +1,4 @@
+#include <queue>
 #include <stack>
 #include <unordered_set>
 
@@ -7,56 +8,58 @@ namespace analyser::metric::metric_impl {
 
 MetricResult::ValueType CodeLinesCountMetric::CalculateImpl(const function::Function &function,
                                                             const ast_extractor::ASTExtractor &ast_extractor) const {
-    std::unordered_set<size_t> res;
-    auto comments = ast_extractor.ExtractAllCommentLineNumbers(function.ast);
+    std::string_view function_ast = function.ast;
 
-    constexpr size_t start_parsing_at = 0;
-    const auto function_data = ast_extractor.ExtractRect(function.ast, start_parsing_at);
-
-    if (!function_data) {
-        return static_cast<MetricResult::ValueType>(res.size());
-    }
-
-    auto [function_rect, continue_parsing_at] = *function_data;
+    std::unordered_set<size_t> lines;
+    auto comment_lines = ast_extractor.ExtractAllCommentLineNumbers(function_ast);
 
     std::stack<ast::LinesInterval> s;
-    s.push(ast::LinesInterval{function_rect});
 
-    while (!s.empty()) {
-        const auto next_data = ast_extractor.ExtractRect(function.ast, continue_parsing_at);
+    constexpr size_t start_parsing_at = 0;
+    std::queue<std::pair<std::string_view, size_t>> q;
 
-        if (!next_data) {
+    q.emplace(function_ast, start_parsing_at);
+
+    while (!q.empty()) {
+        const auto [ast_fragment, continue_parsing_at] = q.front();
+        q.pop();
+
+        const auto rect_data = ast_extractor.ExtractRect(ast_fragment, continue_parsing_at);
+
+        if (rect_data) {
+            const auto [rect, further_parsing_at] = *rect_data;
+            q.emplace(ast_fragment, further_parsing_at);
+
+            const auto current_interval = ast::LinesInterval{rect};
+
             while (!s.empty()) {
-                res.insert(s.top().start_line);
-                s.pop();
+                const auto spanning_interval = s.top();
+
+                if (spanning_interval.Contains(current_interval)) {
+                    break;
+                } else {
+                    lines.insert(spanning_interval.start_line);
+                    lines.insert(spanning_interval.end_line);
+                    s.pop();
+                }
             }
-            break;
-        }
 
-        const auto [next_rect, further_parsing_at] = *next_data;
-        continue_parsing_at = further_parsing_at;
-
-        const auto next_interval = ast::LinesInterval{next_rect};
-
-        while (!s.empty()) {
-            const auto spanning_interval = s.top();
-
-            if (spanning_interval.Contains(next_interval)) {
-                break;
+            if (current_interval.IsOneLine() && comment_lines.contains(current_interval.start_line)) {
+                comment_lines.erase(current_interval.start_line);
             } else {
-                res.insert(spanning_interval.start_line);
+                s.push(current_interval);
+            }
+        } else {
+            while (!s.empty()) {
+                const auto interval = s.top();
+                lines.insert(interval.start_line);
+                lines.insert(interval.end_line);
                 s.pop();
             }
-        }
-
-        if (next_interval.IsOneLine() && comments.contains(next_interval.start_line)) {
-            comments.erase(next_interval.start_line);
-        } else {
-            s.push(next_interval);
         }
     }
 
-    return static_cast<MetricResult::ValueType>(res.size());
+    return static_cast<MetricResult::ValueType>(lines.size());
 }
 
 }  // namespace analyser::metric::metric_impl
