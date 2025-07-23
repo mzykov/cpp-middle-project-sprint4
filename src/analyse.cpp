6427b025d0b7a57971b855142957e7f1cf165708
Mikhail Zykov <zmsmihail@yandex.ru>
2025-07-23 23:48:49 +0300

    Fix after review: replace for_each by pipe notation

--- src/analyse.cpp
+++ src/analyse.cpp
@@ -5,19 +5,15 @@ namespace analyser {
 [[nodiscard]]
 std::vector<std::pair<function::Function, metric::MetricResults>>
 AnalyseFunctions(const std::vector<std::string> &file_names, const metric::MetricExtractor &metric_extractor) {
-    std::vector<std::pair<function::Function, metric::MetricResults>> res;
-
-    std::ranges::for_each(file_names, [&metric_extractor, &res](std::string_view file_name) {
-        const function::FunctionExtractor function_extractor;
-        const auto functions = function_extractor.ProcessOneFile(file::File{file_name});
-
-        std::ranges::for_each(functions, [&function_extractor, &metric_extractor, &res](const auto &function) {
-            const auto metrics = metric_extractor.ProcessOneFunction(function, function_extractor.GetAstExtractor());
-            res.emplace_back(function, metrics);
-        });
-    });
-
-    return res;
+    const function::FunctionExtractor function_extractor;
+    return file_names | std::views::transform([&](std::string_view file_name) {
+               return function_extractor.ProcessOneFile(file::File{file_name});
+           }) |
+           std::views::join | std::views::transform([&](const function::Function &function) {
+               const auto metrics = metric_extractor.ProcessOneFunction(function, function_extractor.GetAstExtractor());
+               return std::pair{function, metrics};
+           }) |
+           std::ranges::to<std::vector<std::pair<function::Function, metric::MetricResults>>>();
 }
 
 }  // namespace analyser
